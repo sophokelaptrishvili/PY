f784a9c79a96910d2e723142b303df4fa25e0f7d
sophokelaptrishvili <127246629+sophokelaptrishvili@users.noreply.github.com>
2023-03-11 22:44:20 +0400

    Create HW4 - Making Faces.py

---
+++ PY/HW4 - Making Faces.py
@@ -0,0 +1,13 @@
+
+message = input("How's your day? Good- :), Bad- :(  ")
+if ":(" in message:
+    print(message.replace(":(", "🙁"))
+
+if ":)" in message:
+    print(message.replace(":)", "🙂 " ))
+
+
+
+
+
+
