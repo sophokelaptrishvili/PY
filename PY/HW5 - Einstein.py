d0a2f1c63c8d19fe92814cc268356793cd2f2e0a
sophokelaptrishvili <127246629+sophokelaptrishvili@users.noreply.github.com>
2023-03-12 23:01:52 +0400

    Create HW5 - Einstein.py

---
+++ PY/HW5 - Einstein.py
@@ -0,0 +1,10 @@
+
+#ask for the input & define the speed of light
+M = int(input("Type the M number so that Einstein can calculate it in Joules for you:"))
+C = 300000000
+
+#create a formula to convert inputted number into Joules
+num = M * C **2
+print(num)
+
+
