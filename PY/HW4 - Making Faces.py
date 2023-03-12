b1e1dd78ad4d8480585d30b18ac4483726ba1be6
sophokelaptrishvili <127246629+sophokelaptrishvili@users.noreply.github.com>
2023-03-12 19:50:46 +0400

    Update HW4 - Making Faces.py

--- PY/HW4 - Making Faces.py
+++ PY/HW4 - Making Faces.py
@@ -1,10 +1,17 @@
+#define the function
+def mainMes():
+    #type the question for the input and give 2 options 
+    message = input("How's your day? Good- :), Bad- :(  ")
 
-message = input("How's your day? Good- :), Bad- :(  ")
-if ":(" in message:
-    print(message.replace(":(", "🙁"))
+    #print specific text according to different criterias
+    if ":(" in message:
+        print("So sorry to hear that you're having a bad day", message.replace(":(", "🙁"), "Everything is going to be allright!💛")
 
-if ":)" in message:
-    print(message.replace(":)", "🙂 " ))
+
+    if ":)" in message:
+        print("Glad to hear that you're having a good day", message.replace(":)", "🙂 " ), "Keep going like this🚀")
+
+mainMes()
 
 
 
