f156d24ced977fe140980acacf0f03601e9f4c29
sophokelaptrishvili <127246629+sophokelaptrishvili@users.noreply.github.com>
2023-03-22 00:00:00 +0400

    Update HW9 - extensions.py

--- PY/HW9 - extensions.py
+++ PY/HW9 - extensions.py
@@ -4,26 +4,26 @@
 fileName = input("write your file name:").lower()
 
 #.gif
-if "gif" in fileName:
+if ".gif" in fileName:
     print("image/gif")
 
 # .jpg # .jpeg
-elif "jpeg" in fileName:
+elif ".jpeg" in fileName:
     print("image/jpeg")
-elif "jpg" in fileName:
+elif ".jpg" in fileName:
     print("image/jpeg")
 
 # .png
-elif "png" in fileName:
+elif ".png" in fileName:
     print("image/png")
 # .pdf
-elif "pdf" in fileName:
+elif ".pdf" in fileName:
     print("application/pdf")
 # .txt
-elif "txt" in fileName:
+elif ".txt" in fileName:
     print("text/plain")
 # .zip
-elif "zip" in fileName:
+elif ".zip" in fileName:
     print("application/zip")
 
 else:
