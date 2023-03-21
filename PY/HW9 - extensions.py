58009256afd24b688c921494ddd25c098d4f31b5
sophokelaptrishvili <127246629+sophokelaptrishvili@users.noreply.github.com>
2023-03-21 23:55:01 +0400

    Create HW9 - extensions.py

---
+++ PY/HW9 - extensions.py
@@ -0,0 +1,30 @@
+
+
+#input
+fileName = input("write your file name:").lower()
+
+#.gif
+if "gif" in fileName:
+    print("image/gif")
+
+# .jpg # .jpeg
+elif "jpeg" in fileName:
+    print("image/jpeg")
+elif "jpg" in fileName:
+    print("image/jpeg")
+
+# .png
+elif "png" in fileName:
+    print("image/png")
+# .pdf
+elif "pdf" in fileName:
+    print("application/pdf")
+# .txt
+elif "txt" in fileName:
+    print("text/plain")
+# .zip
+elif "zip" in fileName:
+    print("application/zip")
+
+else:
+    print("application/octet-stream")
